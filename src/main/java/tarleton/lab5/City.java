2fb604f5246c94f98a3e756c6bb1108142bf4596
Joseph Meier <joseph.meier@go.tarleton.edu>
2021-09-16 21:29:54 -0600

    File Input added

--- src/main/java/tarleton/lab5/City.java
+++ src/main/java/tarleton/lab5/City.java
@@ -2,12 +2,13 @@ package tarleton.lab5;
 
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Objects;
 
 /**
  *
- * @author Joe Meier
+ * @author Joseph Meier
  */
-public class City {
+public class City implements Comparable<City> {
     private final String name;
     private final List<Road> roads;
 
@@ -26,5 +27,37 @@ public class City {
             r.print();
         });
     }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 29 * hash + Objects.hashCode(this.name);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final City other = (City) obj;
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public int compareTo(City o) {
+        return this.name.compareTo(o.name);
+    }
+    
+    
     
 }
--- src/main/java/tarleton/lab5/Main.java
+++ src/main/java/tarleton/lab5/Main.java
@@ -1,15 +1,41 @@
 package tarleton.lab5;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 /**
  *
- * @author Joe Meier
+ * @author Joseph Meier
  */
 public class Main {
     public static void main(String[] args) {
-        Road r1 = new Road(102, "Waco");
-        Road r2 = new Road(195, "Dallas");
-        City c1 = new City("Austin");
-        c1.add(r1);
-        c1.add(r2);
-        c1.print();       
+        try {
+        Path pc = Paths.get("input.txt");
+        Charset cs = Charset.forName("UTF-8");
+        List<String> lines = Files.readAllLines(pc,cs);
+        Set<City> cities = new HashSet<>();
+        for (String line : lines) {
+            String [] s = line.split(",");
+            City c = new City(s[0]);
+            for(int i = 1; i < s.length; i++){
+                Road r = new Road(Integer.parseInt(s[i]),s[i+1]);
+                c.add(r);
+                i++;
+            }
+            cities.add(c);
+        }
+        
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
+
     }
 }
+
